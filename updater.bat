7d2e7a896d4f8700ae865e666aaf658aa1594607
leyuskckiran1510 <kdhakal1510@gmail.com>
2024-05-11 00:09:30 +0545

    update submodules 00:09:30

--- updater.bat
+++ updater.bat
@@ -1,1 +1,11 @@
-git submodule update --init --remote --recursive && git commit -a -m "update submodules $(date +%T)" && git push origin main
+set -x
+
+git submodule update --init --remote --recursive
+
+if [[ -n $? ]];then
+    git commit -a -m "update submodules $(date +%T)"
+
+    if [[ -n $? ]];then
+        git push origin main
+    fi
+fi
