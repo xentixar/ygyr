8a3351e61ecdddd16e3682e3968d3fc44dbab2ef
leyuskckiran1510 <kdhakal1510@gmail.com>
2024-05-11 01:02:23 +0545

    feat:update updater

--- updater.bash
+++ updater.bash
@@ -2,10 +2,10 @@ set -x
 
 git submodule update --init --remote --recursive
 
-if [[ $? -eq 0 ]];then
+if [[ $? -eq '0' ]];then
     git commit -a -m "update submodules $(date +%T)"
 
-    if [[ $? -eq 0 ]];then
+    if [[ $? -eq '0' ]];then
         git push origin main
     fi
 fi
